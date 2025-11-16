b93c1632353da7915cb82ecd42b5cb012a7eb033
DeveloperMaxmudjon <MahmudjonCoder9182>
2025-11-16 10:46:57 +0500

    Test

---
+++ assents/JavaScript/joinMenu.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".joinNow").onclick = function() {
+    document.getElementById("joinMenu").style.top = "90px";
+};
+
+document.querySelector(".closeJoinMenu").onclick = function() {
+    document.getElementById("joinMenu").style.top = "-1500px";
+};
